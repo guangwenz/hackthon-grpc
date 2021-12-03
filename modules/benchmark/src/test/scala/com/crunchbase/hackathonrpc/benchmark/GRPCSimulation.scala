34cd7ad78b581826d7b658ef65f00af386c631ea
Gregg Hernandez <ghernandez@crunchbase.com>
2021-12-03 10:12:43 -0800

    Combine gatling tests into one

--- modules/benchmark/src/test/scala/com/crunchbase/hackathonrpc/benchmark/GRPCSimulation.scala
+++ modules/benchmark/src/test/scala/com/crunchbase/hackathonrpc/benchmark/GRPCSimulation.scala
@@ -9,7 +9,18 @@ class GRPCSimulation extends Simulation {
   val rpc = scenario("RPC").exec(repeat(Benchmark.userRequests, "n") {
     exec(http("grpc").get("/rpc/hello/test#{n}"))
   })
+  val rest = scenario("REST GET").exec(repeat(Benchmark.userRequests, "n") {
+    exec(http("rest-get").get("/rest/hello/test#{n}"))
+  })
+  val restPost =
+    scenario("REST POST").exec(repeat(Benchmark.userRequests, "n") {
+      exec(http("rest-post").get("/rest-post/hello/test#{n}"))
+    })
 
-  setUp(rpc.inject(atOnceUsers(Benchmark.userCount)))
+  setUp(
+    rpc.inject(atOnceUsers(Benchmark.userCount)),
+    rest.inject(atOnceUsers(Benchmark.userCount)),
+    restPost.inject(atOnceUsers(Benchmark.userCount))
+  )
     .protocols(httpProtocol)
 }
--- modules/benchmark/src/test/scala/com/crunchbase/hackathonrpc/benchmark/RESTPostSimulation.scala
+++ modules/benchmark/src/test/scala/com/crunchbase/hackathonrpc/benchmark/RESTPostSimulation.scala
@@ -1,14 +1,14 @@
-package com.crunchbase.hackathonrpc.benchmark
+// package com.crunchbase.hackathonrpc.benchmark
 
-import io.gatling.core.Predef._
-import io.gatling.http.Predef._
+// import io.gatling.core.Predef._
+// import io.gatling.http.Predef._
 
-class RESTPostSimulation extends Simulation {
-  val httpProtocol = http.baseUrl(Benchmark.host)
+// class RESTPostSimulation extends Simulation {
+//   val httpProtocol = http.baseUrl(Benchmark.host)
 
-  val rest = scenario("REST-POST").exec(repeat(Benchmark.userRequests, "n") {
-    exec(http("rest").get("/rest-post/hello/test#{n}"))
-  })
+//   val rest = scenario("REST-POST").exec(repeat(Benchmark.userRequests, "n") {
+//     exec(http("rest").get("/rest-post/hello/test#{n}"))
+//   })
 
-  setUp(rest.inject(atOnceUsers(Benchmark.userCount)).protocols(httpProtocol))
-}
+//   setUp(rest.inject(atOnceUsers(Benchmark.userCount)).protocols(httpProtocol))
+// }
