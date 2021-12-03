f917f3748339e587c571dd86424b41836647557f
Gregg Hernandez <ghernandez@crunchbase.com>
2021-12-03 10:18:41 -0800

    Revert "Combine gatling tests into one"
    This reverts commit 34cd7ad78b581826d7b658ef65f00af386c631ea.

--- modules/benchmark/src/test/scala/com/crunchbase/hackathonrpc/benchmark/RESTSimulation.scala
+++ modules/benchmark/src/test/scala/com/crunchbase/hackathonrpc/benchmark/RESTSimulation.scala
@@ -1,14 +1,14 @@
-// package com.crunchbase.hackathonrpc.benchmark
+package com.crunchbase.hackathonrpc.benchmark
 
-// import io.gatling.core.Predef._
-// import io.gatling.http.Predef._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
 
-// class RESTSimulation extends Simulation {
-//   val httpProtocol = http.baseUrl(Benchmark.host)
+class RESTSimulation extends Simulation {
+  val httpProtocol = http.baseUrl(Benchmark.host)
 
-//   val rest = scenario("REST").exec(repeat(Benchmark.userRequests, "n") {
-//     exec(http("rest").get("/rest/hello/test#{n}"))
-//   })
+  val rest = scenario("REST").exec(repeat(Benchmark.userRequests, "n") {
+    exec(http("rest").get("/rest/hello/test#{n}"))
+  })
 
-//   setUp(rest.inject(atOnceUsers(Benchmark.userCount)).protocols(httpProtocol))
-// }
+  setUp(rest.inject(atOnceUsers(Benchmark.userCount)).protocols(httpProtocol))
+}
